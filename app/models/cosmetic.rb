95273b79e03db3548598a71686039ce1f27a15eb
Laura Brooks <laura.brooks0@outlook.com>
2022-02-12 15:41:10 +0900

    gem fil for favoirtor added

--- app/models/cosmetic.rb
+++ app/models/cosmetic.rb
@@ -1,4 +1,5 @@
 class Cosmetic < ApplicationRecord
   has_many :reviews
   #has_many :ingredients
+  acts_as_favoritable
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  acts_as_taggable_on :tags
+  acts_as_taggable_on :cosmetics
+  acts_as_favoritor
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 end
