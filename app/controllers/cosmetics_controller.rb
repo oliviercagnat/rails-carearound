2f12fa595b45bb3edbab4d976960d0ff9fe9e32c
Olivier Cagnat <oliviercagnat@gmail.com>
2022-02-15 23:08:09 +0900

    adding like button

--- app/controllers/cosmetics_controller.rb
+++ app/controllers/cosmetics_controller.rb
@@ -2,9 +2,15 @@ class CosmeticsController < ApplicationController
 
   before_action :set_cosmectic, only: %i[show]
 
+  # The user must be logged in to like a cosmetic.
+  # The toggle_favorite action is called if the user is logged in,
+  # using the Devise helper.
+  before_action :authenticate_user!, only: :toggle_favorite
+
   def index
     # The first 5 is temporary
     @cosmetics = policy_scope(Cosmetic).first(5)
+    @favorite_cosmetics = current_user.favorited_by_type('Cosmetic')
   end
 
   def show
@@ -32,4 +38,10 @@ class CosmeticsController < ApplicationController
     params.require(:cosmetic).permit(:cosmetic_image, :name, :description, :brand, :average_price, :category, :ingredient_id, :tag_list)
   end
 
+  def toggle_favorite
+    # The toggle_favorite action finds the cosmetic to like through the id parameter.
+    @cosmetic = Cosmetic.find_by(id: params[:id])
+    # It checks if a user has liked it. If it’s been favourited before, it is now unfavorited and vice versa.
+    current_user.favorited?(@cosmetic) ? current_user.unfavorite(@cosmetic) : current_user.favorite(@cosmetic)
+  end
 end
