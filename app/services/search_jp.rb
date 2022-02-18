4a1cd051b6ad332fdd4f083dc4700161267686e8
Laetitia Taddei <laetdi@gmail.com>
2022-02-18 12:25:23 +0900

    parsing products description

--- app/services/search_jp.rb
+++ app/services/search_jp.rb
@@ -6,8 +6,11 @@ class SearchJp
     url = "https://www.matsukiyo.co.jp/store/online/search?category=00331703"
     html_file = URI.open(url).read
     html_doc = Nokogiri::HTML(html_file)
-    product = html_doc.search('#itemList .ttl')
-    product_info = product.text.strip
-    link = product.children[0]['href']
+    results = html_doc.search('#itemList .ttl')
+    cosme = {}
+    results.each do |element|
+      p element.text.strip
+    end
+
   end
 end
