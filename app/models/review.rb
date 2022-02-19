89830a6920c6aaa4de3636179a7f8382bd66de06
David Flanik <daveflanik@gmail.com>
2022-02-19 12:41:57 +0900

    Reviews are finally working. I will work on having them inserted and displayed on the page

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ApplicationRecord
+  belongs_to :cosmetic
   belongs_to :user
   validates :content, :rating, :shop_url, presence: true
   # look into active record validation page to set rating from 1..5 (not super important)
