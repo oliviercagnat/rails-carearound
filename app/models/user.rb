7b2c7bd263363878e590cd482bc39f73f8ed870a
Olivier Cagnat <oliviercagnat@gmail.com>
2022-02-20 15:24:02 +0900

    amend edit login and subscribe, amend profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :name, :avatar, :skin_type, :country, presence: true
+  validates :name, :skin_type, :country, presence: true
+  has_one_attached :avatar
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   acts_as_favoritor
