ba2ff4836e5d30e3864aa87494269f1c226fbc10
David Flanik <daveflanik@gmail.com>
2022-02-21 11:48:38 +0900

    I added interactive stars to the form so users can select stars insted of a number. The form still needs some work but I will get that done and the js.erb file completed soon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,10 @@ import "bootstrap";
 //import 'jquery'
 //import 'select2'
 
+import { initStarRating } from '../plugins/init_star_rating';
+
+initStarRating();
+
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
