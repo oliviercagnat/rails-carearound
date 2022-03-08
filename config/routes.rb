24df853d56fe6a50de5ca42303dd13c83dd092cb
Laetitia Taddei <laetdi@gmail.com>
2022-03-08 22:06:57 +0900

    display my product on the compare page then display research results, showing the ingredients of the product we just created, saving the category of the created product, display the ingredients tags in the show page

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,11 @@ Rails.application.routes.draw do
 
   get 'cosmetics/search', to: 'cosmetics#search', as: :scan
   post 'cosmetics/search', to: 'cosmetics#create'
-  #get 'cosmetics//compare', to: 'cosmetics#compare', as: :compare
-
-
 
   get 'pages/user_profile' => 'pages#user_profile'
 
   # users can leave a review about a cosmetic and display them on cosmetic's page
-  resources :cosmetics, except: [ :destroy ] do
+  resources :cosmetics, except: [:destroy] do
     # The member scopes ensure that the id of the favourite is passed into the action,
     # while the method post ensures that the action will be sent securely to the db and not replicated on page refresh.
     # The generated route will look like this: cosmetics/:id/toggle_favorite
@@ -19,14 +16,12 @@ Rails.application.routes.draw do
       post 'toggle_favorite', to: "cosmetics#toggle_favorite"
       get 'compare', to: 'cosmetics#compare'
       get 'confirm', to: 'cosmetics#confirm'
-
     end
 
-    resources :reviews, only: [ :new, :create ]
+    resources :reviews, only: %i[new create]
   end
 
   get 'pages/user_profile', to: 'pages#user_profile'
-  # get 'cosmetics/search?keyword', to: 'cosmetics#search', as: :search
   # later on displaying ingredients info
   # resources :ingredients, only: [ :show ] ?? how to grab ingredients info ??
 end
